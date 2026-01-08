0cafbf9a63b2583768b63df805eb9c9ccb071f14
a49742812-lgtm <a49742812@gmail.com>
2026-01-08 19:34:38 +0100

    deploy to vercel

--- server.js
+++ server.js
@@ -81,7 +81,4 @@ app.get("/api/auth/callback", async (req, res) => {
 /**
  * ✅ تشغيل السيرفر
  */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
